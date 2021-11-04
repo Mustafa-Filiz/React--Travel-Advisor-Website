501f08a9e9c89df6fae21cdff4306e2ec32d956f
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-04 16:46:54 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
-import { CssBaseline } from '@mui/material';
+import { Grid } from '@mui/material';
 import { Box } from '@mui/system';
-import './App.css';
 import Header from './components/Header';
+import Map from './components/Map';
+import List from './components/List';
+import { useEffect, useState } from 'react';
+import { getPlacesData } from './api/TravelAdvisorApi';
 
 function App() {
-  return (
-    <Box className="App">
-      <CssBaseline />
-      <Header />
-    </Box>
-  );
+    const [type, setType] = useState('restaurants');
+    const [isLoading, setIsLoading] = useState(false);
+    const [places, setPlaces] = useState([]);
+    const [childClicked, setChildClicked] = useState(null);
+    const [coords, setCoords] = useState({});
+    const [bounds, setBounds] = useState(null)
+
+    useEffect(() => {
+        setIsLoading(true);
+        getPlacesData(type).then((data) => {
+            setPlaces(
+                data.filter((place) => place.name && place.num_reviews > 0)
+            );
+            setIsLoading(false);
+        });
+    }, [type]);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            ({ coords: { latitude, longitude } }) => {
+                setCoords({ lat: latitude, lng: longitude });
+            }
+        );
+    }, []);
+
+    return (
+        <Box className="App">
+            <Header />
+            <Grid container sx={{ width: '100%' }}>
+                <Grid item xs={12} md={4}>
+                    <List
+                        type={type}
+                        setType={setType}
+                        childClicked={childClicked}
+                        isLoading={isLoading}
+                        places={places}
+                    />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map places={places} coords={coords} />
+                </Grid>
+            </Grid>
+        </Box>
+    );
 }
 
 export default App;
