750cae0834c4c0a532f01d6a6a6ded7e871acdc1
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-04 16:43:14 +0300

    data fetch function created

---
+++ src/api/TravelAdvisorApi.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+export const getPlacesData = async (type, sw, ne) => {
+    try {
+        const response = await axios.get(
+            `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+            {
+                params: {
+                    bl_latitude: '11.847676',
+                    tr_latitude: '12.838442',
+                    bl_longitude: '109.095887',
+                    tr_longitude: '109.149359',
+                },
+				headers: {
+					'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+					'x-rapidapi-key': process.env.REACT_APP_TRAVEL_API_KEY,
+				},
+            }
+        );
+		return response.data.data
+    } catch (error) {
+        console.log(error);
+    }
+};
